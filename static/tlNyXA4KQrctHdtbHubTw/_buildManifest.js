720866cef03a5fa4b323b885252d9e53ce01cd86
Murat Can <muratal0606@gmail.com>
2023-04-13 00:58:09 +0300

    Updates

--- static/tlNyXA4KQrctHdtbHubTw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/253-443b49ff53c1960c.js","static/chunks/pages/index-ee6a3a192640ae91.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
