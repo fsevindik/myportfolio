c089ba21f3eb5b52a15b8a71a15bb9286317cf05
Murat Can <muratal0606@gmail.com>
2023-11-01 15:19:35 +0300

    add page view count

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,14 @@ import WorkExperience from "../../components/WorkExperience";
 const SECTION_DATA = [];
 
 const Home: NextPage = () => {
+  let viewCount = 0;
+
+  // page load times
+  window.onload = () => {
+    viewCount++;
+    console.log(`Page view count: ${viewCount}`);
+  };
+
   return (
     <div
       className="bg-[rgb(36,36,36)] text-white h-screen snap-y 
