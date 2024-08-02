6675c9d985191d722ed35907e4f20d5ee959103e
muratcan23 <muratal0606@gmail.com>
2024-08-02 11:51:31 +0300

    change text

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -87,7 +87,7 @@ const projects: Project[] = [
     image:
       "https://raw.githubusercontent.com/muratcan23/myportfolio/main/public/filmologuser.png?raw=true",
     description:
-      "This project serves as a modern blog page where you can explore the films I have selected. Feel free to visit, critique, and leave comments or notes for me (the admin).",
+      "You can explore the films I have selected. Feel free to visit, critique, and leave comments or notes for me (the admin). Still expanding and upgrading. ",
     url: "https://filmolog.onrender.com/",
   },
 ];
