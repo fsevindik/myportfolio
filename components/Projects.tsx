d3881ea01652bf8575e27b134826ffab5ac0f2ad
muratcan23 <muratal0606@gmail.com>
2024-08-02 04:53:25 +0300

    change img url

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -15,71 +15,69 @@ const projects: Project[] = [
     image:
       "https://res.cloudinary.com/imagist/image/fetch/f_auto/q_auto/c_scale,w_2240/https://todoist.com/static/home/hero/illustration_w_2026.png?_a=ATCqVcY0",
     description:
-      "Reminder app that has Sign in , Log in and Autentication with email. It has a simple and smooth homepage just like Todoist. There is also page for personal infos like name and personal photo to upload . And of course you can add projects and remove them. I have used Chackra UI for styles.You may reach the original site by clicking on picture.",
+      "Reminder app that has Sign in, Log in, and Authentication with email. It has a simple and smooth homepage just like Todoist. There is also a page for personal info like name and photo upload. You can add and remove projects. I used Chakra UI for styles. You may reach the original site by clicking on the picture.",
     url: "https://todoist.com/auth/signup",
   },
   {
     title: "Authentication and Authorization",
     image:
-      "https://raw.githubusercontent.com/muratcan23/myportfolio/main/public/register.png",
-
+      "https://raw.githubusercontent.com/muratcan23/myportfolio/main/public/register.png?raw=true",
     description:
-      "Authentication is a crucial security concept that ensures only authorized users can access resources and perform actions within a system or application. To help users understand authentication terms, it is important to display them clearly and concisely",
+      "Authentication ensures only authorized users can access resources within a system or application. This page helps users understand authentication terms clearly and concisely.",
     url: "https://github.com/users/kod-man/projects/4",
   },
   {
     title: "Setting UserName and Uploading Photo",
     image:
-      "https://raw.githubusercontent.com/muratcan23/myportfolio/main/public/upload.png",
+      "https://raw.githubusercontent.com/muratcan23/myportfolio/main/public/upload.png?raw=true",
     description:
-      "We provide an onboard page for users to personalize their experience by allowing them to upload a photo and choose a username. To pick a username, users can use a text field where they can enter their desired name. We also offer a file upload control that enables users to select an image file from their device to use as their profile picture. To ensure a smooth experience, we provide guidelines on acceptable image formats and sizes for photo uploads. By enabling users to upload a photo and choose a username, we aim to make our website/application more engaging and personal for them. ",
+      "This page allows users to personalize their experience by uploading a photo and choosing a username. It includes guidelines for image formats and sizes to ensure a smooth experience.",
     url: "https://github.com/users/kod-man/projects/4",
   },
   {
     title: "Adding Projects and Removing Them",
     image:
-      "https://raw.githubusercontent.com/muratcan23/myportfolio/main/public/task.png",
+      "https://raw.githubusercontent.com/muratcan23/myportfolio/main/public/task.png?raw=true",
     description:
-      "Our platform allows users to manage their projects by adding or deleting them as needed. Users can add new projects by using a simple form that includes fields for project name, priority level, description, and color selection. The priority level helps users prioritize their projects, while the color selection enables them to categorize their projects by color for easy identification. Users can also delete projects they no longer need with just a few clicks. Our platform provides a seamless and efficient way for users to manage their projects and stay organized. ",
+      "Users can manage their projects by adding or removing them as needed. It includes fields for project details like name, priority, description, and color selection for easy identification.",
     url: "https://github.com/users/kod-man/projects/4",
   },
   {
     title: "Story Estimate Replica",
     image:
-      "https://github.com/muratcan23/myportfolio/blob/main/public/storyestimate.png?raw=true",
+      "https://raw.githubusercontent.com/muratcan23/myportfolio/main/public/storyestimate.png?raw=true",
     description:
-      " 'Story Estimate' is a fundamental concept in agile project management. It involves a collaborative process where team members collectively assess and estimate the complexity or effort required to complete specific tasks or user stories within a project. This practice plays a vital role in efficient project planning and helps teams prioritize and allocate resources effectively",
+      "'Story Estimate' is a collaborative process where team members assess the complexity of tasks. This project helps in agile project management by enabling effective planning and resource allocation.",
     url: "https://www.storyestimate.com/",
   },
-
   {
     title: "Story Estimate Replica",
     image:
-      "https://github.com/muratcan23/myportfolio/blob/main/public/estimatevotes.png?raw=true",
+      "https://raw.githubusercontent.com/muratcan23/myportfolio/main/public/estimatevotes.png?raw=true",
     description:
-      "Inspired by the concept of 'Story Estimate' , the Story Estimate Replica project is a collaborative web application that mimics the process of taking votes from different team members to estimate task complexity. It calculates the average estimate while also providing a disagreement rate, enhancing agile project planning and consensus-building.",
+      "This project mimics the process of taking votes from team members to estimate task complexity. It calculates the average estimate and provides a disagreement rate for better project planning.",
     url: "https://github.com/muratcan23/estimateMe",
   },
   {
     title: "Story Estimate Replica",
     image:
-      "https://github.com/muratcan23/myportfolio/blob/main/public/estimateresults.png?raw=true",
+      "https://raw.githubusercontent.com/muratcan23/myportfolio/main/public/estimateresults.png?raw=true",
     description:
-      " EstimateME includes vote calculation for task estimates, a disagreement rate metric for team consensus, vote tracking, and clear categorization of consensus levels as medium, high, or low. These features empower teams to collaborate effectively and make informed decisions during the estimation process",
+      "EstimateME includes features for vote calculation, disagreement rate metrics, and clear categorization of consensus levels. It helps teams collaborate effectively during the estimation process.",
     url: "https://github.com/muratcan23/estimateMe",
   },
   {
     title: "Personal film/blog page",
     image:
-      "https://github.com/muratcan23/myportfolio/blob/main/public/filmologregister.png",
+      "https://raw.githubusercontent.com/muratcan23/myportfolio/main/public/filmologregister.png?raw=true",
     description:
-      "This project is a personal endeavor where I have applied much of what I have learned and developed using my own skills. It serves as a modern blog page for me, showcasing my development work and my involvement in all aspects of design and development.",
+      "This project is a personal endeavor where I have applied much of what I have learned and developed using my own skills. It serves as a modern blog page for me, showcasing my development work and involvement in all aspects of design and development.",
     url: "https://filmolog.onrender.com/",
   },
   {
     title: "Personal film/blog page",
     image:
-      "https://github.com/muratcan23/myportfolio/blob/main/public/filmologhome.png",
+      "https://raw.githubusercontent.com/muratcan23/myportfolio/main/public/filmologhome.png?raw=true",
     description:
       "I've utilized many of the skills and knowledge I've acquired to build this project. It reflects my personal touch and showcases my hands-on experience in every aspect of its design and development.",
     url: "https://filmolog.onrender.com/",
@@ -87,7 +85,7 @@ const projects: Project[] = [
   {
     title: "Personal film/blog page",
     image:
-      "https://github.com/muratcan23/myportfolio/blob/main/public/filmologuser.png",
+      "https://raw.githubusercontent.com/muratcan23/myportfolio/main/public/filmologuser.png?raw=true",
     description:
       "This project serves as a modern blog page where you can explore the films I have selected. Feel free to visit, critique, and leave comments or notes for me (the admin).",
     url: "https://filmolog.onrender.com/",
@@ -127,7 +125,8 @@ function Projects({}: Props) {
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true }}
                 src={project.image}
-                alt=""
+                alt={project.title}
+                className="w-full h-auto object-cover"
               />
             </a>
             <div className="space-y-10 px-0 md:px-10 max-w-6xl mb-auto">
