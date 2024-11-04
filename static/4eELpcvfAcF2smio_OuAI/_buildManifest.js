8dc919b7cee30412661bd3935501e2948f009883
fsevindik <fsevindik2923@gmail.com>
2024-11-04 17:25:17 +0300

    Updates

---
+++ static/4eELpcvfAcF2smio_OuAI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/191-8fa5c2179575e295.js","static/chunks/pages/index-fe8a282cd01e8feb.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
